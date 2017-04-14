14a4243fd40e491997ae5e1e573df582c21ff7de
RickR <rrein@akamai.com>
2017-04-13 21:40:34 -0600

    Fixes secret path

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.lograge.enabled = true
+
+  config.secret_path = '/run/secrets'
 end
