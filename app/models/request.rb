a1b42d40f5c03e20446f4d59420f177d89136ab2
Chris Mar <cmar@customink.com>
2017-05-19 15:01:48 -0400

    add belongs_to service for request

--- app/models/request.rb
+++ app/models/request.rb
@@ -2,10 +2,9 @@ class Request < ApplicationRecord
   belongs_to :user
   belongs_to :requested_mentor, class_name: 'User', optional: true
   belongs_to :assigned_mentor, class_name: 'User', optional: true
-  # belongs_to :service
+  belongs_to :service
 
   validates_presence_of :user
-  # validates_presence_of :service
 
   scope :unclaimed, -> { where(assigned_mentor: nil) }
 end
