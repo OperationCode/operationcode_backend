0a6e35696518544605585e9a357aca123c3ec117
Chris Mar <cmar@customink.com>
2017-05-19 13:37:47 -0400

    add factory girl and faker

--- test/models/request_test.rb
+++ test/models/request_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class RequestTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    user = create(:user)
+    p user
+  end
 end
