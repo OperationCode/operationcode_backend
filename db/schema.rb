0e8b4df3a6ac2a28253b3fc5f34b3da42d205d69
Chris Mar <cmar@customink.com>
2017-05-19 11:26:22 -0400

    add service request model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170519144023) do
+ActiveRecord::Schema.define(version: 20170519145444) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "requests", force: :cascade do |t|
+    t.integer  "service_id"
+    t.string   "language"
+    t.text     "details"
+    t.integer  "user_id"
+    t.integer  "assigned_mentor_id"
+    t.integer  "requested_mentor_id"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.index ["assigned_mentor_id"], name: "index_requests_on_assigned_mentor_id", using: :btree
+    t.index ["requested_mentor_id"], name: "index_requests_on_requested_mentor_id", using: :btree
+    t.index ["service_id"], name: "index_requests_on_service_id", using: :btree
+    t.index ["user_id"], name: "index_requests_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
@@ -41,4 +56,5 @@ ActiveRecord::Schema.define(version: 20170519144023) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "requests", "users"
 end
