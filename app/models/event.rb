0fcf2b5d68022fef1845861f85d6c8050af6f858
Colleen <colleen.schnettler@gmail.com>
2017-06-15 08:30:27 -0400

    Final commit adding events endpoint with index action and validations in model.

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,11 +8,7 @@ class Event < ApplicationRecord
 	validates :state, presence: true, length: {is: 2} #Dropdown selection on front end?  
 	VALID_ZIP_REGEX= /\A\d{5}-?\d{4}?\z/
 	validates :zip, presence: true, format:{with:VALID_ZIP_REGEX}
-	#This validation doesn't work the way I think it should.  All strings are converted to true except false.  This does prevent nil, but does not force the text "true" or "false"
 	validates :scholarship_available, inclusion: {in: [true, false]}
-	
-	#This regex only accounts for www. addresses, I assume we won't be linking to ftp sites? 
-	#Does not catch www.. I don't know of any websites starting with ., but it is a valid unreserved character so I didn't want to cut it.  
 	VALID_URL_REGEX = /\Awww\.+.+\.[a-z]+\z/i 
 	validates :URL, format: {with:VALID_URL_REGEX}
 
