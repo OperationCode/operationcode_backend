21e4aa86bbfd6d90cd6360bc1017d9a21c1d2c6f
Harry Levine <hmlevine@gmail.com>
2017-06-21 22:17:15 -0600

    Refactors routes to leverage resources syntax
    Alphabetizes and groups by controller.

--- config/routes.rb
+++ config/routes.rb
@@ -7,15 +7,15 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/status', to: 'status#all'
       get '/status/protected', to: 'status#protected'
-      post '/users', to: 'users#create'
-      get '/code_schools', to: 'code_schools#index'
-      get '/events', to: 'events#index'
 
-      get '/services', to: 'services#index'
+      post '/users', to: 'users#create'
       post '/users/profile/verify', to: 'users#verify'
 
+      resources :code_schools, only: :index
+      resources :events, only: :index
       resources :mentors, only: [:index, :create, :show]
       resources :requests, only: [:index, :create, :show, :update]
+      resources :services, only: :index
       resources :squads, only: [:index, :create, :show] do
         member do
           post 'join'
