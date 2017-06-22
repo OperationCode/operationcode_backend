f51f8226919e3c6db4f9c680907b9957213ced21
Harry Levine <hmlevine@gmail.com>
2017-06-21 22:15:08 -0600

    Adds a verified:boolean column to :users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170616161847) do
+ActiveRecord::Schema.define(version: 20170622013242) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -109,6 +109,7 @@ ActiveRecord::Schema.define(version: 20170616161847) do
     t.string   "last_name"
     t.string   "timezone"
     t.text     "bio"
+    t.boolean  "verified",               default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
