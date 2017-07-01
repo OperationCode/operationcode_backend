7b1bd8944e88ac99bd179081220f759eba6e3273
Harry Levine <hmlevine@gmail.com>
2017-07-01 16:47:30 -0600

    Vote model with associations and validations
    Second validation ensures that a user can only vote for a resource one time.
    
    Also includes Rails counter_caching for how many votes a given resource has.

---
+++ app/models/vote.rb
@@ -0,0 +1,7 @@
+class Vote < ApplicationRecord
+  belongs_to :user
+  belongs_to :resource, counter_cache: true
+
+  validates :user_id, :resource_id, presence: true
+  validates :user_id, uniqueness: { scope: :resource_id, message: "has already voted for this resource" }
+end
