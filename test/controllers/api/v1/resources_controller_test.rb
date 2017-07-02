63919644fd0acf1fa7ac2eaa4899667adcef6362
Harry Levine <hmlevine@gmail.com>
2017-07-02 16:09:08 -0600

    removes user auth on public endpoints

--- test/controllers/api/v1/resources_controller_test.rb
+++ test/controllers/api/v1/resources_controller_test.rb
@@ -10,11 +10,11 @@ class Api::V1::ResourcesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test ":index endpoint returns JSON list of Resources" do
-    get api_v1_resources_url, headers: @headers, as: :json
+    get api_v1_resources_url, as: :json
 
     assert_equal response.status, 200
-    [@books.name, @videos.name].each do |resource|
-      assert_equal true, response.parsed_body.any? { |response| response['name'] == resource }
+    [@books.name, @videos.name].each do |resource_name|
+      assert_equal true, response.parsed_body.any? { |response| response['name'] == resource_name }
     end
   end
 
@@ -26,16 +26,16 @@ class Api::V1::ResourcesControllerTest < ActionDispatch::IntegrationTest
     @videos.tag_list.add 'screencast'
     @videos.save
 
-    get api_v1_resources_url(params), headers: @headers, as: :json
+    get api_v1_resources_url(params), as: :json
 
     assert_equal response.parsed_body.size, 2
-    [blog.name, @videos.name].each do |resource|
-      assert_equal true, response.parsed_body.any? { |response| response['name'] == resource }
+    [blog.name, @videos.name].each do |resource_name|
+      assert_equal true, response.parsed_body.any? { |response| response['name'] == resource_name }
     end
   end
 
   test ":show endpoint returns JSON representation of the requested Resource" do
-    get api_v1_resource_url(@books.id), headers: @headers, as: :json
+    get api_v1_resource_url(@books.id), as: :json
 
     assert_equal response.status, 200
     assert_equal @books.id, response.parsed_body["id"]
