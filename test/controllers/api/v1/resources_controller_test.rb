71b07138ffff2db03fa317a10155bd2c1fb33a59
Harry Levine <hmlevine@gmail.com>
2017-07-01 20:23:58 -0600

    Tests for Resources controller :index and :show endpoints

---
+++ test/controllers/api/v1/resources_controller_test.rb
@@ -0,0 +1,46 @@
+require 'test_helper'
+
+class Api::V1::ResourcesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    user = create(:user)
+    @headers = authorization_headers(user)
+
+    @books = create(:resource)
+    @videos = create(:resource, name: 'Free videos')
+  end
+
+  test ":index endpoint returns JSON list of Resources" do
+    get api_v1_resources_url, headers: @headers, as: :json
+
+    assert_equal response.status, 200
+    [@books.name, @videos.name].each do |resource|
+      assert_equal true, response.parsed_body.any? { |response| response['name'] == resource }
+    end
+  end
+
+  test ":index endpoint returns correct tagged Resources when :tags param is passed" do
+    params = { tags: ['screencast', 'articles'] }
+    blog = create(:resource, name: 'Blog posts')
+    blog.tag_list.add 'articles'
+    blog.save
+    @videos.tag_list.add 'screencast'
+    @videos.save
+
+    get api_v1_resources_url, headers: @headers, params: params, as: :json
+
+    assert_equal response.parsed_body.size, 2
+    [blog.name, @videos.name].each do |resource|
+      assert_equal true, response.parsed_body.any? { |response| response['name'] == resource }
+    end
+  end
+
+  test ":show endpoint returns JSON representation of the requested Resource" do
+    get api_v1_resource_url(@books.id), headers: @headers, as: :json
+
+    assert_equal response.status, 200
+    assert_equal @books.id, response.parsed_body["id"]
+    assert_equal @books.name, response.parsed_body["name"]
+    assert_equal @books.url, response.parsed_body["url"]
+    assert_equal @books.category, response.parsed_body["category"]
+  end
+end
