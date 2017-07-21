549f4ef8d12362d3ab5a6f22675cffe3de945361
Harry Levine <hmlevine@gmail.com>
2017-07-21 10:39:34 -0600

    Removes required user authentication to access API endpoints

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -1,15 +1,10 @@
 require 'test_helper'
 
 class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    user = create(:user)
-    @headers = authorization_headers(user)
-  end
-
   test ":index returns User.count" do
     2.times { create :user }
 
-    get api_v1_users_url, headers: @headers, as: :json
+    get api_v1_users_url, as: :json
 
     assert_equal({ 'user_count' => User.count }, response.parsed_body)
     assert_equal 200, response.status
@@ -23,20 +18,20 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
     sam.update latitude: 30.312601, longitude: -97.738591
 
     params = { zip: '78705' }
-    get api_v1_users_by_location_url(params), headers: @headers, as: :json
+    get api_v1_users_by_location_url(params), as: :json
     assert_equal({ 'user_count' => 1 }, response.parsed_body)
     assert_equal 200, response.status
 
     params = { zip: '78705, 78756' }
-    get api_v1_users_by_location_url(params), headers: @headers, as: :json
+    get api_v1_users_by_location_url(params), as: :json
     assert_equal({ 'user_count' => 2 }, response.parsed_body)
 
     params = { lat_long: [30.285648, -97.742052] }
-    get api_v1_users_by_location_url(params), headers: @headers, as: :json
+    get api_v1_users_by_location_url(params), as: :json
     assert_equal({ 'user_count' => 2 }, response.parsed_body)
 
     params = { lat_long: [30.285648, -97.742052], radius: 1 }
-    get api_v1_users_by_location_url(params), headers: @headers, as: :json
+    get api_v1_users_by_location_url(params), as: :json
     assert_equal({ 'user_count' => 1 }, response.parsed_body)
   end
 end
