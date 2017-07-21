4aed52356fc84d4f8a0e34ee2eaab13166595603
Harry Levine <hmlevine@gmail.com>
2017-07-21 10:16:38 -0600

    Updates API endpoint specs and docs for new :state param

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -11,11 +11,16 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test ":by_location returns User.count of users located in the passed in location" do
-    tom = create :user, zip: '78705'
-    sam = create :user, zip: '78756'
+    tom = create :user
+    sam = create :user
 
-    tom.update latitude: 30.285648, longitude: -97.742052
-    sam.update latitude: 30.312601, longitude: -97.738591
+    tom.update_columns latitude: 30.285648, longitude: -97.742052, zip: '78705', state: 'TX'
+    sam.update_columns latitude: 30.312601, longitude: -97.738591, zip: '78756', state: 'TX'
+
+    params = { state: 'TX' }
+    get api_v1_users_by_location_url(params), headers: @headers, as: :json
+    assert_equal({ 'user_count' => 2 }, response.parsed_body)
+    assert_equal 200, response.status
 
     params = { zip: '78705' }
     get api_v1_users_by_location_url(params), as: :json
@@ -25,13 +30,16 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
     params = { zip: '78705, 78756' }
     get api_v1_users_by_location_url(params), as: :json
     assert_equal({ 'user_count' => 2 }, response.parsed_body)
+    assert_equal 200, response.status
 
     params = { lat_long: [30.285648, -97.742052] }
     get api_v1_users_by_location_url(params), as: :json
     assert_equal({ 'user_count' => 2 }, response.parsed_body)
+    assert_equal 200, response.status
 
     params = { lat_long: [30.285648, -97.742052], radius: 1 }
     get api_v1_users_by_location_url(params), as: :json
     assert_equal({ 'user_count' => 1 }, response.parsed_body)
+    assert_equal 200, response.status
   end
 end
