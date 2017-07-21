c91416a792135a0864ce65a0646fb479e153dcdb
Harry Levine <hmlevine@gmail.com>
2017-07-21 10:16:10 -0600

    Updates UsersByLocation class to search by :state

---
+++ test/lib/users_by_location_test.rb
@@ -0,0 +1,60 @@
+require 'test_helper'
+
+class UsersByLocationTest < ActiveSupport::TestCase
+  setup do
+    tom = create :user
+    sam = create :user
+
+    tom.update_columns latitude: 39.622200, longitude: -104.889371, zip: '80111', state: 'CO'
+    sam.update_columns latitude: 30.312601, longitude: -97.738591, zip: '78756', state: 'TX'
+  end
+
+  test '#count by state' do
+    params  = { state: 'CO' }
+    results = UsersByLocation.new(params).count
+    assert_equal 1, results
+
+    params  = { state: 'TX' }
+    results = UsersByLocation.new(params).count
+    assert_equal 1, results
+
+    params  = { state: 'CO, TX' }
+    results = UsersByLocation.new(params).count
+    assert_equal 2, results
+  end
+
+  test '#count by zip' do
+    params  = { zip: '80111' }
+    results = UsersByLocation.new(params).count
+    assert_equal 1, results
+
+    params  = { zip: '78756' }
+    results = UsersByLocation.new(params).count
+    assert_equal 1, results
+
+    params  = { zip: '80111, 78756' }
+    results = UsersByLocation.new(params).count
+    assert_equal 2, results
+  end
+
+  test '#count by city' do
+    params  = { city: 'Greenwood Village, CO, US' }
+    results = UsersByLocation.new(params).count
+    assert_equal 1, results
+
+    params  = { city: 'Austin, TX, US' }
+    results = UsersByLocation.new(params).count
+    assert_equal 1, results
+  end
+
+  test '#count by lat_long' do
+    params  = { lat_long: [39.622200, -104.889371] }
+    results = UsersByLocation.new(params).count
+    assert_equal 1, results
+
+    params  = { lat_long: [30.312601, -97.738591] }
+    results = UsersByLocation.new(params).count
+    assert_equal 1, results
+  end
+end
+
