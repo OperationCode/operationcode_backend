34bb511e06eb777a31c6e5762d8bb876653b2a64
Harry Levine <hmlevine@gmail.com>
2017-07-23 09:21:30 -0600

    Updates all associated tests

--- test/lib/format_data_test.rb
+++ test/lib/format_data_test.rb
@@ -2,13 +2,13 @@ require 'test_helper'
 
 class FormatDataTest < ActiveSupport::TestCase
   test '.csv_to_array' do
-    results = FormatData.csv_to_array '80202'
-    assert_equal ['80202'], results
+    results = FormatData.csv_to_array 'TX'
+    assert_equal ['tx'], results
+
+    results = FormatData.csv_to_array 'TX, MN'
+    assert_equal ['tx', 'mn'], results
 
     results = FormatData.csv_to_array '80202, 80126'
     assert_equal ['80202', '80126'], results
-
-    results = FormatData.csv_to_array 'TX, MN'
-    assert_equal ['TX', 'MN'], results
   end
 end
