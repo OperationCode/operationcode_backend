98da19f559ec94cc6629a6bd6c300489c05bc808
Julien Mayer <jalexismayer@gmail.com>
2017-08-02 16:44:26 -0500

    adds belongs_to associations

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,6 @@
 class Application < ApplicationRecord
+  belongs_to :user
+  belongs_to :scholarship
   # before_save only verified members can submit
   delegate :name, to: :user
   delegate :email, to: :user
