7b7f3b3a0bb644bc0aa3b65274ed9ba80bc4ae88
Julien Mayer <jalexismayer@gmail.com>
2017-08-03 17:35:18 -0500

    adds validation

--- app/models/scholarship_application.rb
+++ app/models/scholarship_application.rb
@@ -1,7 +1,15 @@
 class ScholarshipApplication < ApplicationRecord
+  validate :user_is_verified
+
   belongs_to :user
   belongs_to :scholarship
-  # before_save only verified members can submit
+
   delegate :name, to: :user
   delegate :email, to: :user
+
+  def user_is_verified
+    unless self.user.verified
+      errors.add(:verified, "Only verified users may submit applications")
+    end
+  end
 end
