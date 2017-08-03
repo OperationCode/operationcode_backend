a574e56756d91e65e69d3ce285d68bc4922a9b0d
Julien Mayer <jalexismayer@gmail.com>
2017-08-03 17:44:38 -0500

    adds migration to rename columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170803215307) do
+ActiveRecord::Schema.define(version: 20170803223854) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,8 +75,8 @@ ActiveRecord::Schema.define(version: 20170803215307) do
     t.text     "description"
     t.string   "location"
     t.text     "terms"
-    t.datetime "start_date"
-    t.datetime "end_date"
+    t.datetime "open_time"
+    t.datetime "close_time"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
