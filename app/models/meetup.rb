c9c2f7af87e4532ed0e83dfb7954e231e09126c3
Colleen <colleen.schnettler@gmail.com>
2017-08-03 20:59:11 -0400

    Simplified meetup.rb

--- app/models/meetup.rb
+++ app/models/meetup.rb
@@ -2,20 +2,29 @@ class Meetup
 	include HTTParty 
 	base_uri 'api.meetup.com'
 
-	def api_key
-		@api_key = '5187488' #Should be an ENV variable 
-	end
+	# def api_key
+	# 	@api_key = '5187488' #Should be an ENV variable 
+	# end
 
-	def api_key_two
-		@api_key_two = '05b5d92d7a520168cf978bc769da4fdb4630f0e9'
-	end
+	# def api_key_two
+	# 	@api_key_two = '05b5d92d7a520168cf978bc769da4fdb4630f0e9'
+	# end
 
-	def base_path 
-		"/operation-code-hampton-roads/events?photo-host=public&page=20&sig_id=#{api_key}&sig=#{api_key_two}"
-	end
+	# def base_path 
+	# 	"/operation-code-hampton-roads/events?photo-host=public&page=20&sig_id=#{api_key}&sig=#{api_key_two}"
+	# end
 
-	def get_events
-		self.class.get(base_path).parsed_response
-	end
+	# def get_events
+	# 	self.class.get(base_path).parsed_response
+	# end
+	# #url should be /pro/operation-code/events/.....
+
+  def base_path
+  	"/operation-code-hampton-roads/events?photo-host=public&page=20"
+
+  end
+  def get_events
+  	self.class.get(base_path).parsed_response
+  end
 
 end 
