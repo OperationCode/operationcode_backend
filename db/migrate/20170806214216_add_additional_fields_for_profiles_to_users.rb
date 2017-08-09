255ffb91c5d1cbeedbf956d8b1bbbeec172244bb
Evan Cooper <evanncooper@gmail.com>
2017-08-09 08:05:19 -0400

    Update 20170806214216_add_additional_fields_for_profiles_to_users.rb

--- db/migrate/20170806214216_add_additional_fields_for_profiles_to_users.rb
+++ db/migrate/20170806214216_add_additional_fields_for_profiles_to_users.rb
@@ -4,5 +4,7 @@ class AddAdditionalFieldsForProfilesToUsers < ActiveRecord::Migration[5.0]
     add_column :users, :company_name, :string
     add_column :users, :education_level, :string
     add_column :users, :interests, :string
+    add_column :users, :scholarship_info :boolean
+    add_column :users, :interests, :string, array: true, default: []
   end
 end
