d2beeb961f823f8e7ccbeec5ffde56b8321a181f
Harry Levine <hmlevine@gmail.com>
2017-08-29 20:16:00 -0600

    GitHubStatistic model with scopes, validations, callbacks, and methods

---
+++ app/models/git_hub_statistic.rb
@@ -0,0 +1,141 @@
+class GitHubStatistic < ApplicationRecord
+  PR = 'PullRequest'
+  ISSUE = 'Issue'
+  COMMIT = 'Commit'
+
+  belongs_to :git_hub_user
+
+  validates :git_hub_user_id, :source_type, :source_id, :repository, :url, :title, :completed_on, presence: true
+  validates :source_id, uniqueness: { scope: :source_type }
+
+  before_save :downcase_source_id, :downcase_repository
+
+  scope :pull_requests, -> { where(source_type: GitHubStatistic::PR) }
+  scope :issues, -> { where(source_type: GitHubStatistic::ISSUE) }
+  scope :commits, -> { where(source_type: GitHubStatistic::COMMIT) }
+  scope :closed, -> { where(state: 'closed') }
+  scope :open, -> { where(state: 'open') }
+  scope :for_git_hub_user, -> git_hub_user { where(git_hub_user_id: git_hub_user.id) }
+  scope :for_repository, -> repository { where(repository: repository) }
+
+  # For the most recently closed pull request, gets its :completed_on date.
+  # If none exist, returns '1970-01-01'
+  #
+  # @return [String] A date in its string format
+  #
+  def self.last_pr_completed_on
+    date = self
+      .pull_requests
+      .last_closed_date
+
+    default_date date
+  end
+
+  # For the most recently closed pull request, gets its :completed_on date.
+  # If none exist, returns '1970-01-01'
+  #
+  # @return [String] A date in its string format
+  #
+  def self.last_issue_completed_on
+    date = self
+      .issues
+      .last_closed_date
+
+    default_date date
+  end
+
+  # Returns the unique count of GitHubUsers that have made a commit.
+  #
+  # @return [Integer]
+  #
+  def self.users_made_a_commit
+    self
+      .commits
+      .pluck(:git_hub_user_id)
+      .uniq
+      .size
+  end
+
+  # Returns the unique count of GitHubUsers that have a closed pull request.
+  #
+  # @return [Integer]
+  #
+  def self.users_closed_a_pr
+    self
+      .closed
+      .pull_requests
+      .pluck(:git_hub_user_id)
+      .uniq
+      .size
+  end
+
+  # Fetches records that have a :completed_on Date bewtween the passed in start
+  # and end dates.  Start date defaults to 1/1/1970.  End date defaults to today.
+  #
+  # @return [GitHubStatistic] ActiveRecord Collection of GitHubStatistics
+  #
+  def self.date_range(start_date: '1970-01-01', end_date: Date.today.to_s)
+    where completed_on: (start_date..end_date)
+  end
+
+  def self.respositories
+    @respositories ||= pluck(:repository).uniq
+  end
+
+  # Returns the unique count of repositories we have statistics on.
+  #
+  # @return [Integer]
+  #
+  def self.repository_count
+    @repository_count ||= respositories.size
+  end
+
+  def self.average_closed_prs_per_user
+    grouped_records = pull_requests.closed.group_by(&:git_hub_user_id)
+
+    return 0 unless grouped_records.present?
+
+    per_user_average_in grouped_records
+  end
+
+  def self.average_commits_prs_per_user
+    grouped_records = commits.group_by(&:git_hub_user_id)
+
+    return 0 unless grouped_records.present?
+
+    per_user_average_in grouped_records
+  end
+
+  private
+
+  def self.per_user_average_in(grouped_records)
+    per_group_totals = []
+
+    grouped_records.each do |key, value|
+      per_group_totals << value.length
+    end
+
+    per_group_totals.sum / per_group_totals.size.to_f
+  end
+
+  def self.last_closed_date
+    self
+      .closed
+      .order(completed_on: :desc)
+      .first
+      .try(:completed_on)
+      .to_s
+  end
+
+  def self.default_date(date)
+    date.present? ? date : '1970-01-01'
+  end
+
+  def downcase_source_id
+    source_id.downcase!
+  end
+
+  def downcase_repository
+    repository.downcase!
+  end
+end
---
+++ test/models/git_hub_statistic_test.rb
@@ -0,0 +1,80 @@
+require 'test_helper'
+
+class GitHubStatisticTest < ActiveSupport::TestCase
+  test ".last_pr_completed_on returns the most recently closed pull request's :completed_on date, else '1970-01-01'" do
+    GitHubStatistic.delete_all
+    assert_equal '1970-01-01', GitHubStatistic.last_pr_completed_on
+
+    statistic = create :git_hub_statistic
+    assert_equal statistic.completed_on.to_s, GitHubStatistic.last_pr_completed_on
+  end
+
+  test ".last_issue_completed_on returns the most recently closed issues's :completed_on date, else '1970-01-01'" do
+    GitHubStatistic.delete_all
+    assert_equal '1970-01-01', GitHubStatistic.last_issue_completed_on
+
+    statistic = create :git_hub_statistic, :issue, state: 'closed'
+    assert_equal statistic.completed_on.to_s, GitHubStatistic.last_issue_completed_on
+  end
+
+  test ".users_made_a_commit returns the unique count of users that have made a commit" do
+    GitHubStatistic.delete_all
+
+    stan, david, beth = create_users 3
+
+    create :git_hub_statistic, :commit, git_hub_user: stan
+    create :git_hub_statistic, :commit, git_hub_user: stan
+    create :git_hub_statistic, :commit, git_hub_user: david
+    create :git_hub_statistic, git_hub_user: beth
+
+    assert_equal GitHubStatistic.users_made_a_commit, 2
+  end
+
+  test ".users_closed_a_pr returns the unique count of users that have closed a pull request" do
+    GitHubStatistic.delete_all
+
+    stan, david, beth = create_users 3
+
+    create :git_hub_statistic, git_hub_user: stan
+    create :git_hub_statistic, git_hub_user: stan
+    create :git_hub_statistic, git_hub_user: david
+    create :git_hub_statistic, :commit, git_hub_user: beth
+
+    assert_equal GitHubStatistic.users_closed_a_pr, 2
+  end
+
+  test ".repository_count returns the unique count of repositories we have statistics on" do
+    GitHubStatistic.delete_all
+
+    create :git_hub_statistic, repository: 'uno'
+    create :git_hub_statistic, repository: 'uno'
+    create :git_hub_statistic, repository: 'dos'
+
+    assert_equal GitHubStatistic.repository_count, 2
+  end
+
+  test ".date_range returns records that have a :completed_on Date bewtween the passed in start and end dates" do
+    GitHubStatistic.delete_all
+
+    twenty_10 = create :git_hub_statistic, completed_on: '2010-01-15'
+    twenty_14 = create :git_hub_statistic, :commit, completed_on: '2014-01-15'
+    twenty_17 = create :git_hub_statistic, :issue, completed_on: '2017-01-15'
+
+    assert_equal GitHubStatistic.date_range, [twenty_10, twenty_14, twenty_17]
+    assert_equal GitHubStatistic.date_range(start_date: '2014-01-15'), [twenty_14, twenty_17]
+    assert_equal GitHubStatistic.date_range(start_date: '2017-01-15'), [twenty_17]
+    assert_equal GitHubStatistic.date_range(end_date: '2014-01-14'), [twenty_10]
+    assert_equal GitHubStatistic.date_range(end_date: '2009-01-15'), []
+    assert_equal GitHubStatistic.date_range(start_date: '2014-01-10', end_date: '2014-01-19'), [twenty_14]
+  end
+
+  def create_users(number)
+    users = []
+
+    number.times do
+      users << create(:git_hub_user)
+    end
+
+    users
+  end
+end
