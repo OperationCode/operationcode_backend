077f8f1b8a786412ce3e248d00c9f4ae599b3bbf
Loi Tran <lvt15@my.fsu.edu>
2017-09-26 23:07:55 +0700

    Corrected locations Factory

--- test/factories/locations.rb
+++ test/factories/locations.rb
@@ -1,11 +1,11 @@
 FactoryGirl.define do
   factory :location do
-    va_accepted false
-    address1 "MyString"
-    address2 "MyString"
-    city "MyString"
-    state "MyString"
-    zip 1
+    va_accepted Faker::Boolean.boolean
+    address1 Faker::Address.street_address
+    address2 Faker::Address.street_address
+    city Faker::Address.city
+    state Faker::Address.state
+    zip Faker::Address.zip_code
     code_school nil
   end
 end
