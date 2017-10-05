a5e3dbc63c5dd5905f1c97fdd6ccd755c269e197
Harry Levine <hmlevine@gmail.com>
2017-10-05 10:47:04 -0600

    Fixes test failure based on test db’s code school count
    The fix was done on lines 31 - 34.
    
    By opening the file in sublime, it also corrected a bunch of blank space issues.

--- test/controllers/api/v1/code_schools_controller_test.rb
+++ test/controllers/api/v1/code_schools_controller_test.rb
@@ -1,56 +1,55 @@
 require 'test_helper'
 
 class Api::V1::CodeSchoolsControllerTest < ActionDispatch::IntegrationTest
-  setup do 
+  setup do
     @school = create(:code_school)
     @school.name = "CoderSchool"
     @school.save
   end
-  
-  test ":validates CodeSchool's required fields" do 
+
+  test ":validates CodeSchool's required fields" do
     params = {
       code_school: {
         name: "CoderSchool"
       }
-    }    
+    }
     post api_v1_code_schools_url, params: params, as: :json
-    
+
     assert JSON.parse(response.body)["errors"].include? "Url can't be blank"
   end
-  
-  test ":index endpoint returns a JSON list of all CodeSchools" do 
-    get api_v1_code_schools_path, as: :json    
+
+  test ":index endpoint returns a JSON list of all CodeSchools" do
+    get api_v1_code_schools_path, as: :json
     assert_equal JSON.parse(response.body)[0]["name"], "CoderSchool"
   end
-  
-  test ":create endpoint creates a CodeSchool successfully" do    
+
+  test ":create endpoint creates a CodeSchool successfully" do
     post api_v1_code_schools_path(@school), params: {code_school: @school}, as: :json
     assert_response :ok
   end
-  
+
   test ":show will not work for a invalid record" do
-    school_count = CodeSchool.count + 1
-    get api_v1_code_school_path(school_count), as: :json
+    get api_v1_code_school_path(99999999), as: :json
     assert_response :missing
   end
-  
+
   test ":show works for a valid record"do
     get api_v1_code_school_path(@school), as: :json
     assert_response :ok
   end
-  
+
   test ":update endpoint updates an existing CodeSchool" do
     put api_v1_code_school_path(@school), params: {name: "CoddderrrrSchool"}, as: :json
     assert_equal response.status, 200
     assert_equal JSON.parse(response.body)["name"], "CoddderrrrSchool"
   end
-  
+
   test ":destroy endpoint destroys an existing CodeSchool" do
     id = @school.id
-    
-    delete api_v1_code_school_path(@school)    
+
+    delete api_v1_code_school_path(@school)
     assert_equal response.status, 200
-    
+
     get api_v1_code_school_path(id), as: :json
     assert_response :missing
   end
