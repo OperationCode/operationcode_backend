bcaa39c7588838f0a7ac3b8bc855d6255bbac836
Harry Levine <hmlevine@gmail.com>
2017-10-22 20:11:47 -0600

    DRY’s up GitHubUser assembly

--- app/lib/git_hub/structure.rb
+++ app/lib/git_hub/structure.rb
@@ -19,13 +19,7 @@ module GitHub
         url: response['html_url'],
         title: response['title'],
         closed_on: response['merged_at'].to_date,
-        git_hub_user: {
-          login: response['user']['login'],
-          avatar_url: response['user']['avatar_url'],
-          api_url: response['user']['url'],
-          html_url: response['user']['html_url'],
-          id: response['user']['id'],
-        }
+        git_hub_user: append_git_hub_user('user')
       }
     end
 
@@ -37,13 +31,7 @@ module GitHub
         title: response['commit']['message'],
         repository: repository,
         closed_on: response['commit']['committer']['date'].to_date,
-        git_hub_user: {
-          login: response['author']['login'],
-          avatar_url: response['author']['avatar_url'],
-          api_url: response['author']['url'],
-          html_url: response['author']['html_url'],
-          id: response['author']['id'],
-        }
+        git_hub_user: append_git_hub_user('author')
       }
     end
 
@@ -57,13 +45,19 @@ module GitHub
         state: response['state'],
         repository: repository,
         closed_on: response['closed_at'].to_date,
-        git_hub_user: {
-          login: response['assignee']['login'],
-          avatar_url: response['assignee']['avatar_url'],
-          api_url: response['assignee']['url'],
-          html_url: response['assignee']['html_url'],
-          id: response['assignee']['id'],
-        }
+        git_hub_user: append_git_hub_user('assignee')
+      }
+    end
+
+  private
+
+    def append_git_hub_user(user_key)
+      {
+        login: response[user_key]['login'],
+        avatar_url: response[user_key]['avatar_url'],
+        api_url: response[user_key]['url'],
+        html_url: response[user_key]['html_url'],
+        id: response[user_key]['id'],
       }
     end
   end
