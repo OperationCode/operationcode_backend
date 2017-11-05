40f8f18d444ccf4b48d8585e1b825bc3fcaba311
Harry Levine <hmlevine@gmail.com>
2017-11-05 16:02:40 -0700

    Sets the sidekiq database URL to be dynamic with a fallback

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,8 @@
 require 'sidekiq'
 
 Sidekiq.configure_server do |config|
+  ENV['DATABASE_URL'] = ENV['POSTGRES_HOST'].present? ? ENV['POSTGRES_HOST'] : 'operationcode-psql'
+
   Rails.logger = Sidekiq::Logging.logger
   ActiveRecord::Base.logger = Sidekiq::Logging.logger
 end
