081d287f94d751155fc45dfa04e4d14eae65331b
Harry Levine <hmlevine@gmail.com>
2017-11-05 16:11:23 -0700

    Adding yard docs

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,10 @@
 require 'sidekiq'
 
+# Setting the ENV['DATABASE_URL'] explicitly to allow for use of a
+# dynamic database host variable ENV['POSTGRES_HOST']
+#
+# @see https://github.com/mperham/sidekiq/wiki/Advanced-Options#rails-32-and-newer
+#
 Sidekiq.configure_server do |config|
   ENV['DATABASE_URL'] = ENV['POSTGRES_HOST'].present? ? ENV['POSTGRES_HOST'] : 'operationcode-psql'
 
