450bb359bf5023085a47f3096144c631515f5da8
PixelWitch <21322920+PixelWitch@users.noreply.github.com>
2017-11-20 15:41:15 -0500

    fixed user creation in social

--- app/models/user.rb
+++ app/models/user.rb
@@ -103,13 +103,14 @@ class User < ApplicationRecord
     user = User.where(email: data[:email]).first
 
     # Uncomment the section below if you want users to be created if they don't exist
-     unless user
-         user = User.create(first_name: data[:first_name],
+    unless user
+         user = User.new(first_name: data[:first_name],
             last_name: data[:last_name],
-            email: data[:email]
+            email: data[:email],
+            password: Devise.friendly_token[0,20]
          )
-     end
-     user
+    end
+    user
   end
 
   private
