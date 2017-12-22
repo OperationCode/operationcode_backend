2ced8bb20609e2371ee6aafda437724fc7ba9e51
Harry Levine <hmlevine@gmail.com>
2017-12-22 11:49:47 -0700

    ActiveJob perform_later does not accept a Struct wrapped in an Array
    All of the tests were giving false positives, because in testing, `perform_now` is called in the background.
    
    Also, during manual testing, perform_now was being used.
    
    The catch is, perform_later does not accept an array wrapped Struct as an argument, despite the fact that is allows arrays.
    
    As such, this logic was migrated from the controller, to the job itself.
    
    Now the job is receiving a string as as an arg, which perform_later does accept.

--- app/controllers/api/v1/email_list_recipients_controller.rb
+++ app/controllers/api/v1/email_list_recipients_controller.rb
@@ -4,7 +4,7 @@ module Api
       def create
         raise "Invalid email address: #{permitted_params[:email]}" unless valid_email?
 
-        AddUserToSendGridJob.perform_later(guest)
+        AddUserToSendGridJob.perform_later(nil, guest_email: permitted_params[:email])
 
         render json: { email: permitted_params[:email], guest: true }, status: :created
       rescue StandardError => e
@@ -15,17 +15,6 @@ module Api
 
       def permitted_params
         params.permit(:email)
-      end
-
-      # Guest is a Struct.  ActiveJob does not support the passing of Structs
-      # as an argument. An ActiveJob::SerializationError < ArgumentError is raised.
-      #
-      # It does permit Arrays.  As such, the Struct is being passed within an Array.
-      #
-      # @see http://api.rubyonrails.org/classes/ActiveJob/SerializationError.html
-      #
-      def guest
-        [SendGridClient::Guest.user(permitted_params[:email])]
       end
 
       def valid_email?
