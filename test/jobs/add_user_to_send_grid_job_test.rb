e564f48861100e5d7a7de66e82d511ef413864c1
Harry Levine <hmlevine@gmail.com>
2017-12-29 20:03:33 -0700

    Removes guest option for AddUserToSendGridJob

--- test/jobs/add_user_to_send_grid_job_test.rb
+++ test/jobs/add_user_to_send_grid_job_test.rb
@@ -8,24 +8,4 @@ class AddUserToSendGridJobTest < ActiveJob::TestCase
 
     AddUserToSendGridJob.perform_now(user)
   end
-
-  test "with a guest_email, and no user, it adds the Struct user to send grid" do
-    guest = SendGridClient::Guest.user(valid_email)
-
-    SendGridClient.any_instance.expects(:add_user).with(guest)
-
-    AddUserToSendGridJob.perform_now(nil, valid_email)
-  end
-
-  test "with a guest_email, and a valid user, it adds the User to send grid" do
-    user = FactoryGirl.build(:user)
-
-    SendGridClient.any_instance.expects(:add_user).with(user)
-
-    AddUserToSendGridJob.perform_now(user, valid_email)
-  end
-end
-
-def valid_email
-  "john@gmail.com"
 end
