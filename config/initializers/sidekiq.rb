9af317f2993101cae9266f72a845a887a4a85843
Harry Levine <hmlevine@gmail.com>
2017-12-31 11:23:00 -0700

    Resolves issues with local fatal errors around postgres
    We were getting this in the local server logs:
    
    operationcode-psql_1  | FATAL:  database "operationcode-psql" does not exist
    operationcode-psql_1  | FATAL:  database "operationcode-psql" does not exist
    operationcode-psql_1  | FATAL:  database "operationcode-psql" does not exist
    operationcode-psql_1  | FATAL:  database "operationcode-psql" does not exist
    
    Specifying this variable in the .env file resolved it

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -7,15 +7,6 @@ require 'sidekiq'
 #
 Sidekiq.configure_server do |config|
   ENV['DATABASE_URL'] = ENV['POSTGRES_HOST'].present? ? ENV['POSTGRES_HOST'] : 'operationcode-psql'
-  database_url = ENV['DATABASE_URL']
-
-  if database_url
-    ENV['DATABASE_URL'] = "#{database_url}?pool=25"
-    ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
-    # Note that as of Rails 4.1 the `establish_connection` method requires
-    # the database_url be passed in as an argument. Like this:
-    # ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
-  end
 
   Rails.logger = Sidekiq::Logging.logger
   ActiveRecord::Base.logger = Sidekiq::Logging.logger
