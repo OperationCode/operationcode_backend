aa89e376a3eea2607a3cb0e915af6c8839d85e6a
Alex Beeken <aeb242@gmail.com>
2018-01-29 23:44:36 -0800

    fix typo

--- app/models/team_member.rb
+++ app/models/team_member.rb
@@ -1,6 +1,6 @@
 class TeamMember < ApplicationRecord
   BOARD_GROUP_NAME = "board"
-  EXECUTIVE_GROUP_NAME = "board"
+  EXECUTIVE_GROUP_NAME = "executive"
   TEAM_GROUP_NAME = "team"
   validates :name, :role, :group, presence: true
   validates :group, inclusion: {
