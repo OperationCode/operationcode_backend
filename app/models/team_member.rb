d83713f64b14fa0a7677d51a15c4a9782de5ec23
Alex Beeken <aeb242@gmail.com>
2018-01-30 22:02:14 -0800

    freeze constants

--- app/models/team_member.rb
+++ app/models/team_member.rb
@@ -1,7 +1,7 @@
 class TeamMember < ApplicationRecord
-  BOARD_GROUP_NAME = "board"
-  EXECUTIVE_GROUP_NAME = "executive"
-  TEAM_GROUP_NAME = "team"
+  BOARD_GROUP_NAME = "board".freeze
+  EXECUTIVE_GROUP_NAME = "executive".freeze
+  TEAM_GROUP_NAME = "team".freeze
   validates :name, :role, :group, presence: true
   validates :group, inclusion: {
     in: [BOARD_GROUP_NAME, EXECUTIVE_GROUP_NAME, TEAM_GROUP_NAME]
