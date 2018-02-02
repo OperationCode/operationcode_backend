aae8ad7dc486d78f50c1ce4ea6c40ea1b5643850
Alex Beeken <aeb242@gmail.com>
2018-02-01 17:17:50 -0800

    remove accidentally committed migrations

--- db/migrate/20180129223616_add_group_to_team_members.rb
+++
@@ -1,5 +0,0 @@
-class AddGroupToTeamMembers < ActiveRecord::Migration[5.0]
-  def change
-    add_column :team_members, :group, :text
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180129223616) do
+ActiveRecord::Schema.define(version: 20171022215418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -176,9 +176,10 @@ ActiveRecord::Schema.define(version: 20180129223616) do
   create_table "team_members", force: :cascade do |t|
     t.string   "name"
     t.string   "role"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.text     "group"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "description"
+    t.string   "image_src"
   end
 
   create_table "users", force: :cascade do |t|
