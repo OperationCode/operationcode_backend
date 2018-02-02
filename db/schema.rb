d39bfeb5018ba9d53ffaf6706cd07d4aa192dfa4
Alex Beeken <aeb242@gmail.com>
2018-02-01 17:18:47 -0800

    remove more accidental commits

--- db/schema.rb
+++ db/schema.rb
@@ -176,10 +176,6 @@ ActiveRecord::Schema.define(version: 20171022215418) do
   create_table "team_members", force: :cascade do |t|
     t.string   "name"
     t.string   "role"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.text     "description"
-    t.string   "image_src"
   end
 
   create_table "users", force: :cascade do |t|
