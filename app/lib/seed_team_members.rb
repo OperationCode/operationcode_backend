b5ed7537ee25e0fd86ddf8756487c7b280fc35c4
Alex Beeken <aeb242@gmail.com>
2018-02-05 17:02:51 -0800

    fix code climate issues and clean up a little bit

--- app/lib/seed_team_members.rb
+++ app/lib/seed_team_members.rb
@@ -1,11 +1,38 @@
 class SeedTeamMembers
   BOARD_DATA_PATH = 'board_members.yml'.freeze
   TEAM_DATA_PATH = 'team_members.yml'.freeze
+  class << self
 
-  def self.from_yaml(file_name, group)
-    members_seed_file = Rails.root.join('config', file_name)
-    members = YAML.load_file(members_seed_file)
-    members.map do |member|
+    def from_yaml(file_name, group)
+      members_seed_file = Rails.root.join('config', file_name)
+      members = YAML.load_file(members_seed_file)
+      members.map do |member|
+        create_or_update(member, group)
+      end
+    end
+
+    def seed_all
+      seed_board
+      seed_team
+    end
+
+    def seed_board
+      from_yaml(
+        BOARD_DATA_PATH,
+        TeamMember::BOARD_GROUP_NAME
+      )
+    end
+
+    def seed_team
+      from_yaml(
+        TEAM_DATA_PATH,
+        TeamMember::TEAM_GROUP_NAME
+      )
+    end
+
+    private
+
+    def create_or_update(member, group)
       TeamMember.find_or_create_by!(name: member['name']) do |c|
         c.description = member['description'] if member['description']
         c.image_src = member['image_src'] if member['image_src']
@@ -14,24 +41,5 @@ class SeedTeamMembers
         c.group = group
       end
     end
-  end
-
-  def self.seed_all
-    seed_board
-    seed_team
-  end
-
-  def self.seed_board
-    from_yaml(
-      BOARD_DATA_PATH,
-      TeamMember::BOARD_GROUP_NAME
-    )
-  end
-
-  def self.seed_team
-    from_yaml(
-      TEAM_DATA_PATH,
-      TeamMember::TEAM_GROUP_NAME
-    )
   end
 end
