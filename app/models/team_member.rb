b5d182802d7f269e8396b3d263e84696c16d7837
Alex Beeken <aeb242@gmail.com>
2018-02-05 16:19:40 -0800

    add space between constants and validations

--- app/models/team_member.rb
+++ app/models/team_member.rb
@@ -2,6 +2,7 @@ class TeamMember < ApplicationRecord
   BOARD_GROUP_NAME = "board".freeze
   EXECUTIVE_GROUP_NAME = "executive".freeze
   TEAM_GROUP_NAME = "team".freeze
+
   validates :name, :role, :group, presence: true
   validates :group, inclusion: {
     in: [BOARD_GROUP_NAME, EXECUTIVE_GROUP_NAME, TEAM_GROUP_NAME]
