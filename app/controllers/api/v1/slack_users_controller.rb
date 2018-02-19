64da21b3565f3b87ba91fdafcbcf216ab5e603d3
wimo7 <wimo7083@colorado.edu>
2018-02-19 11:29:46 -0700

    changed status to render in header

--- app/controllers/api/v1/slack_users_controller.rb
+++ app/controllers/api/v1/slack_users_controller.rb
@@ -34,11 +34,9 @@ module Api
       def update
         user = User.find(params[:email])
         if user.update(slack_name params[:slack_name])
-          render json: { user.attributes,
-                         status: 200}
+          render json: { user.attributes}, status: 200
         else
-          render json: { errors: user.errors.full_messages,
-                         status:  400}
+          render json: { errors: user.errors.full_messages}, status: 400
         end
       end
     
