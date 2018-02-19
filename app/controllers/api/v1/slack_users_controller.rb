418c47c02b12cc07af0a66db419d89d160650ae4
wimo7 <wimo7083@colorado.edu>
2018-02-19 09:50:39 -0700

    added status code for update

--- app/controllers/api/v1/slack_users_controller.rb
+++ app/controllers/api/v1/slack_users_controller.rb
@@ -34,9 +34,11 @@ module Api
       def update
         user = User.find(params[:email])
         if user.update(slack_name params[:slack_name])
-          render json: user 
+          render json: { user.attributes,
+                          status: 200}
         else
-          render json: { errors: user.errors.full_messages }
+          render json: { errors: user.errors.full_messages,
+                         status:  400}
         end
       end
     
