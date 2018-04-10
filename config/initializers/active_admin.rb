e7efce346b64dbcb7dbecf262fd6de68fd878858
hpjaj <hmlevine@gmail.com>
2018-04-09 19:21:35 -0600

    Adds CanCanCan gem
    The ActiveAdmin Pundit adapter does not work and is not being actively supported.
    
    Also, we only use Pundit in one resource, and that endpoint is not active from the FE.  So it will be refactored/removed with regards to auth.

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -62,7 +62,7 @@ ActiveAdmin.setup do |config|
   # method in a before filter of all controller actions to
   # ensure that there is a user with proper rights. You can use
   # CanCanAdapter or make your own. Please refer to documentation.
-  # config.authorization_adapter = ActiveAdmin::CanCanAdapter
+  config.authorization_adapter = ActiveAdmin::CanCanAdapter
 
   # In case you prefer Pundit over other solutions you can here pass
   # the name of default policy class. This policy will be used in every
@@ -77,7 +77,7 @@ ActiveAdmin.setup do |config|
   # because, by default, user gets redirected to Dashboard. If user
   # doesn't have access to Dashboard, he'll end up in a redirect loop.
   # Method provided here should be defined in application_controller.rb.
-  # config.on_unauthorized_access = :access_denied
+  config.on_unauthorized_access = :access_denied
 
   # == Current User
   #
