679364ea9a7725f7d17a73b45b25de74f1fd92d3
hpjaj <hmlevine@gmail.com>
2018-04-22 08:06:18 -0600

    PyBot auth through passing encoded JWT
    This will be decoded, and then compared to the py bot auth key env var.
    
    This can be tested in dev, and used in prod, as each env has unique keys.

--- app/controllers/api/v1/slack_users_controller.rb
+++ app/controllers/api/v1/slack_users_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class SlackUsersController < ApiController
-      before_action :authenticate_user!
+      before_action :authenticate_py_bot!
 
       def create
         raise "User id #{current_user.id} does not have a saved email address." unless current_user.email.present?
@@ -11,6 +11,31 @@ module Api
         render json: { status: :ok }
       rescue StandardError => e
         render json: { errors: e.message }, status: :unprocessable_entity
+      end
+
+      def access
+        render json: { message: 'You have access!' }, status: :ok
+      end
+
+      private
+
+      def authenticate_py_bot!
+        auth = JsonWebToken.decode(auth_token)
+
+        if auth['key'] == Rails.application.secrets.py_bot_auth_key
+          true
+        else
+          render json: { errors: ["Auth token is incorrect"] }, status: :unauthorized
+        end
+
+      rescue JWT::ExpiredSignature
+        render json: { errors: ["Auth token has expired"] }, status: :unauthorized
+      rescue JWT::DecodeError
+        render json: { errors: ["Invalid auth token"] }, status: :unauthorized
+      end
+
+      def auth_token
+        @auth_token ||= request.headers.fetch('Authorization', '').split(' ').last
       end
     end
   end
