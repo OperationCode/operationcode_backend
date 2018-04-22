dc32e3945728797f54492acde3cbb2144c330296
wimo7083 <wimo7083@colorado.edu>
2018-04-22 12:42:25 -0600

    changed quotes again

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -1,14 +1,14 @@
 FactoryGirl.define do
   factory :user do
     email { Faker::Internet.email }
-    zip "97201"
+    zip '97201'
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     latitude { Faker::Number.decimal(2, 5) }
     longitude { Faker::Number.decimal(2, 5) }
     password { Faker::Lorem.characters(32) }
     bio { Faker::Company.bs }
-    timezone "EST"
+    timezone 'EST'
     mentor false
     verified false
     state { Faker::Address.state_abbr }
