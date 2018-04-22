2d708254ace319abcb3a0e0625f304715a6a2bb7
wimo7083 <wimo7083@colorado.edu>
2018-04-21 21:23:49 -0600

    remove droped column from serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :zip, :first_name, :last_name,
-             :slack_name, :mentor, :verified
+             :mentor, :verified
   attributes :role
 
   def role
