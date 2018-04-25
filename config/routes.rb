d399431b0c3ed14af97b10621fff7db903428ff9
kirbygit <heybisayangdako@gmail.com>
2018-04-24 21:33:16 -0700

    removing changes but test failed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-  mount ForestLiana::Engine => '/forest'
   devise_for :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
