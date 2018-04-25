ee35b78afb006458d7c42433addd2342ea174b4f
nagano564 <nagano564@gmail.com>
2018-04-24 22:22:35 -0400

    fixed schema conficts

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20180416024105) do
-=======
 ActiveRecord::Schema.define(version: 20180420192231) do
->>>>>>> migration completed
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
