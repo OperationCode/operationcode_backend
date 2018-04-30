a2a13a5a7fc6be1430f87442ea25828ef0cbc657
nagano564 <nagano564@gmail.com>
2018-04-30 14:15:20 -0400

    added rep_name/email to code_schools. finished all checkpoints except the active admin resource

--- app/serializers/code_school_serializer.rb
+++ app/serializers/code_school_serializer.rb
@@ -1,5 +1,5 @@
 class CodeSchoolSerializer < ActiveModel::Serializer
-  attributes :name, :url, :logo, :full_time, :hardware_included, :has_online, :online_only, :mooc, :is_partner
+  attributes :name, :url, :logo, :full_time, :hardware_included, :has_online, :online_only, :mooc, :is_partner, :rep_name, :rep_email
   has_many :locations
 
   def locations
