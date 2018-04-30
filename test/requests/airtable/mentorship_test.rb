6bc4676ee157b55c7e6abcc7e1451cafc49bd525
hpjaj <hmlevine@gmail.com>
2018-04-29 19:24:46 -0600

    Specs for the happy path when fetching mentor request data

---
+++ test/requests/airtable/mentorship_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class MentorshipTest < Minitest::Test
+  def setup
+    VCR.use_cassette('airtable/mentorship/successful') do
+      @successful_response = Airtable::Mentorship.new.mentor_request_data
+    end
+  end
+
+  def test_mentor_request_data_returns_correct_keys
+    assert @successful_response.keys.sort == [:mentors, :services, :skillsets]
+  end
+
+  def test_mentor_request_data_returns_correct_mentor_data
+    mentor = @successful_response[:mentors].first
+
+    assert mentor[:id].present?
+    assert mentor[:full_name].present?
+  end
+
+  def test_mentor_request_data_returns_correct_service_data
+    service = @successful_response[:services].first
+
+    assert service[:id].present?
+    assert service[:name].present?
+  end
+
+def test_mentor_request_data_returns_correct_skillset_data
+    skillset = @successful_response[:skillsets].first
+
+    assert skillset[:id].present?
+    assert skillset[:name].present?
+  end
+end
