69210cc3076d973fd8507cf23be66991c8709afb
hpjaj <hmlevine@gmail.com>
2018-05-06 14:09:35 -0600

    Method to create a record a new Mentor Request in Airtable

--- app/lib/airtable/mentorship.rb
+++ app/lib/airtable/mentorship.rb
@@ -19,17 +19,23 @@ module Airtable
       }
     end
 
-    private
-
-    def mentors
-      mentors = client.get_records_for('Mentors')
-
-      mentors.map do |mentor|
-        {
-          id: mentor['id'],
-          full_name: mentor.dig('fields', 'Full Name')
+    # Creates a new mentor request record in the Mentor Request Airtable
+    #
+    # @param body [Hash] Hash of mentor request attributes
+    # @return [Hash] Hash of the newly created mentor request Airtable record
+    #
+    def create_mentor_request(body)
+      request_body = {
+        "fields" => {
+          "Slack User" => body[:slack_user],
+          "Service" => format_for_posting(body[:services]),
+          "Skillsets" => format_for_posting(body[:skillsets]),
+          "Additional Details" => body[:additional_details],
+          "Mentor Requested" => format_for_posting(body[:mentor_requested])
         }
-      end
+      }.to_json
+
+      client.post_record('Mentor Request', request_body)
     end
 
     private
@@ -45,6 +51,13 @@ module Airtable
       end
     end
 
+    # Converts a comman separated string into an array of strings
+    #
+    # @param data_string [String] Comma separated strings of data
+    # @return [Array] An array of stripped strings
+    #
+    def format_for_posting(data_string)
+      data_string.split(",").map(&:strip)
     end
   end
 end
