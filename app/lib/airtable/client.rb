40c0c9a1c6e47f9f3cac6e98966c1bed11a98348
hpjaj <hmlevine@gmail.com>
2018-05-06 14:07:24 -0600

    Calls HTTParty directly and cleans up URL

--- app/lib/airtable/client.rb
+++ app/lib/airtable/client.rb
@@ -1,25 +1,46 @@
-require "httparty"
-
 module Airtable
   class Client
     include HTTParty
-    base_uri 'api.airtable.com/v0'
 
-    attr_reader :base_id, :headers
+    attr_reader :base_id, :headers, :base_url
 
     def initialize
-      api_key  = Rails.application.secrets.airtable_api_key
-      @base_id = Rails.application.secrets.airtable_base_id
-      @headers = { :Authorization => "Bearer #{api_key}"}
+      api_key   = Rails.application.secrets.airtable_api_key
+      @base_id  = Rails.application.secrets.airtable_base_id
+      @headers  = { :Authorization => "Bearer #{api_key}"}
+      @base_url = "https://api.airtable.com/v0/#{base_id}/"
     end
 
+    # Fetches all of the records for the passed Airtable
+    #
+    # @param table [String] Name of the table in Airtable
+    # @return [Hash] The parsed_response from Airtable
+    #
     def get_records_for(table)
-      response = self.class.get("/#{base_id}/#{table}", headers: headers)
+      response = HTTParty.get(url_for(table), headers: headers)
+
+      return_value_for(response)
+    end
 
       return_value_for(response)
     end
 
     private
+
+    def url_for(table)
+      base_url + escaped(table)
+    end
+
+    # URL-encodes the passed string for cases where the string has spaces
+    #
+    # @param url_string [String] a URL string
+    # @return [String] String where any spaces are URL-encoded
+    # @example escaped('Mentor Request') => 'Mentor%20Request'
+    # @see http://ruby-doc.org/stdlib-2.3.3/libdoc/uri/rdoc/URI/Escape.html
+    #
+    def escaped(url_string)
+      URI.escape url_string
+    end
 
     # If the Airtable rate limit is reached, a 429 status code is returned
     # @see https://airtable.com/appSqQz7spgg0I1jQ/api/docs#curl/ratelimits
@@ -27,7 +48,7 @@ module Airtable
     def return_value_for(response)
       case response.code
       when 200
-        response.parsed_response['records']
+        response.parsed_response
       when 429
         raise Airtable::Error, 'Exceeded Airtable rate limit. Wait 30 seconds for subsequent requests.'
       else
