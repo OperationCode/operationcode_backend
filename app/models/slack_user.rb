16f4e95030ceb261adb08edfa2fc42af80a6e6ea
wimo7083 <wimo7083@colorado.edu>
2018-05-14 19:29:23 -0600

    added some validation

--- app/models/slack_user.rb
+++ app/models/slack_user.rb
@@ -1,6 +1,3 @@
 class SlackUser < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  validates :slack_id, :slack_name, :slack_real_name, :slack_email, :user_id, presence: true
 end
