4dc1ffa01b94a9e86156c321f169653e34cd2977
Kenny Nagano <nagano564@gmail.com>
2018-05-21 01:27:01 -0400

    Scholarship_application belongs_to Scholarship

--- test/factories/scholarship_applications.rb
+++ test/factories/scholarship_applications.rb
@@ -3,5 +3,6 @@ FactoryGirl.define do
     reason 'MyText'
     terms_accepted false
     association :user, factory: :user, verified: true
+    association :scholarship, factory: :scholarship
   end
 end
