6f022f03247aa8194e6b5319f4ac149c1ce5d8d3
Colleen Schnettler <leenyburger@users.noreply.github.com>
2018-06-01 13:56:51 -0400

    Removed extraneous comments

--- app/admin/job.rb
+++ app/admin/job.rb
@@ -1,7 +1,4 @@
 ActiveAdmin.register Job do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
   permit_params :title, :source_url, :source, :city, :state, :country, :description, :status, :remote
 
   index do
