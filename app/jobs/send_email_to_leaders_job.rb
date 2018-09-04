7c04c68503e88728671f3fc15bd50fe170e14765
wimo7083 <wimo7083@colorado.edu>
2018-09-04 16:38:56 -0600

    reduce indentation

--- app/jobs/send_email_to_leaders_job.rb
+++ app/jobs/send_email_to_leaders_job.rb
@@ -2,6 +2,6 @@ class SendEmailToLeadersJob < ActiveJob::Base
   queue_as :default
 
   def perform(_user_id)
-      logger.debug 'Deprecated pathway, trying to determine what is placing this on queue.'
+    logger.debug 'Deprecated pathway, trying to determine what is placing this on queue.'
   end
 end
