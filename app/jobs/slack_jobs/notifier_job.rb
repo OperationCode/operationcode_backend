b1570c5298d4605f8f05b304723554c2d160ffdc
William Montgomery <10781353+wimo7083@users.noreply.github.com>
2018-09-04 07:56:56 -0600

    Revert "Bug: more error fixing attempts. "

--- app/jobs/slack_jobs/notifier_job.rb
+++ app/jobs/slack_jobs/notifier_job.rb
@@ -2,8 +2,9 @@ class SlackJobs
   class SlackNotifierJob < SlackJobs
     queue_as :default
 
-    def perform(_message, *)
-      logger.debug 'Deprecated pathway, trying to determine what is placing this on queue.'
+    def perform(message, channel: '#general')
+      res = slack_client.post_message_to channel: channel, with_text: message
+      Rails.logger.debug("Notified channel: #{res.inspect}")
     end
   end
 end
