e4dd4363f13ac2dfa597df26362fd8286490de48
wimo7083 <wimo7083@colorado.edu>
2018-09-04 20:25:57 -0600

    remove DATABASE_URL override, setting to wrong format

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,12 +1,6 @@
 require 'sidekiq'
 
-# Setting the ENV['DATABASE_URL'] explicitly to allow for use of a
-# dynamic database host variable ENV['POSTGRES_HOST']
-#
-# @see https://github.com/mperham/sidekiq/wiki/Advanced-Options#rails-32-and-newer
-#
 Sidekiq.configure_server do |config|
-  ENV['DATABASE_URL'] = ENV['POSTGRES_HOST'].present? ? ENV['POSTGRES_HOST'] : 'operationcode-psql'
   # https://stackoverflow.com/questions/28412913/disable-automatic-retry-with-activejob-used-with-sidekiq
   # prevent excessive retries from some default status.
   config.death_handlers << lambda { |job, e|
