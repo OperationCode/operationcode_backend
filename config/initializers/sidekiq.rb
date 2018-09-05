e17816131cadae76dc2e2ecde5b7fbf6ce304f6f
wimo7083 <wimo7083@colorado.edu>
2018-09-04 19:34:42 -0600

    readd death handler

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -7,7 +7,13 @@ require 'sidekiq'
 #
 Sidekiq.configure_server do |config|
   ENV['DATABASE_URL'] = ENV['POSTGRES_HOST'].present? ? ENV['POSTGRES_HOST'] : 'operationcode-psql'
-
+  # https://stackoverflow.com/questions/28412913/disable-automatic-retry-with-activejob-used-with-sidekiq
+  # prevent excessive retries from some default status.
+  config.death_handlers << lambda { |job, e|
+    # thanks rob for the nice message
+    evt = Raven::Event.new(message: "#{job['class']} #{job['jid']} done run outta tries.")
+    Raven.send_event(evt)
+  }
   Rails.logger = Sidekiq::Logging.logger
   ActiveRecord::Base.logger = Sidekiq::Logging.logger
 end
