728433861550752fa410622cc7109324bf6fef30
Robb Kidd <robb@thekidds.org>
2018-09-13 09:20:52 -0400

    optimize N+1 query in retrieving CodeSchools with locations
    * add a default sort order to the locations association on CodeSchool
    * update CodeSchoolsController to include locations in the query result
      passed to the JSON rendering
    * remove the custom locations() method from CodeSchoolSerializer to make
      the serializer rely on the association and default sort ordering
      already declared
    
    * added a factory for creating a code school with 1-15 locations
    
    Signed-off-by: Robb Kidd <robb@thekidds.org>

--- app/models/code_school.rb
+++ app/models/code_school.rb
@@ -1,5 +1,5 @@
 class CodeSchool < ApplicationRecord
   validates :name, :url, :logo, presence: true
   validates_inclusion_of :full_time, :hardware_included, :has_online, :online_only, :in => [true, false]
-  has_many :locations, dependent: :destroy
+  has_many :locations, -> { order('state ASC, city ASC') }, dependent: :destroy
 end
