c2dae659081f6f53a47f2b742760bd36338e196b
Robb Kidd <robb@thekidds.org>
2018-09-14 11:58:41 -0400

    wrap some factory attrs in blocks to make them lazy
    Laziness in a factory lets an attr be random. Without putting these in
    Ruby blocks, FactoryGirl will call Faker for an attribute once when a
    factory is first used and then use that value for every create() and
    build() thereafter. This is helpful in development for being able to
    generate a bunch of sample data in the database that looks different. It
    will also ensure that the records for the test suite are randomish to
    make sure tests aren't passing because of some underlying, but
    unintentional consistency.
    
    Signed-off-by: Robb Kidd <robb@thekidds.org>

--- test/factories/code_schools.rb
+++ test/factories/code_schools.rb
@@ -1,17 +1,17 @@
 FactoryGirl.define do
   factory :code_school do
-    name Faker::Company.name
-    url Faker::Internet.url
-    logo Faker::Internet.url
-    full_time Faker::Boolean.boolean
-    hardware_included Faker::Boolean.boolean
-    has_online Faker::Boolean.boolean
-    online_only Faker::Boolean.boolean
-    notes Faker::Lorem.paragraph
+    name { Faker::Company.name }
+    url { Faker::Internet.url }
+    logo { Faker::Internet.url }
+    full_time { Faker::Boolean.boolean }
+    hardware_included { Faker::Boolean.boolean }
+    has_online { Faker::Boolean.boolean }
+    online_only { Faker::Boolean.boolean }
+    notes { Faker::Lorem.paragraph }
     mooc false
     is_partner false
-    rep_name Faker::Name.name
-    rep_email Faker::Internet.email
+    rep_name { Faker::Name.name }
+    rep_email { Faker::Internet.email }
 
     factory :code_school_with_locations do
       transient do
