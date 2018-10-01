490e535856e2aba321413a93961abe3d1fe63049
Robb Kidd <robb@thekidds.org>
2018-10-01 11:15:49 -0400

    set VCR to error by default for unknown 3rd party HTTP calls
    Signed-off-by: Robb Kidd <robb@thekidds.org>

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -43,7 +43,7 @@ class ActiveSupport::TestCase
 end
 
 VCR.configure do |config|
-  config.allow_http_connections_when_no_cassette = true
+  config.allow_http_connections_when_no_cassette = false
   config.cassette_library_dir = 'test/cassettes'
   config.hook_into :webmock
 end
