9ea4d554bac0890976165c8d2f75369be5bca144
wimo7083 <wimo7083@colorado.edu>
2018-10-06 14:11:09 -0600

    completed scope and added block scope for mocking

--- app/models/job.rb
+++ app/models/job.rb
@@ -4,6 +4,14 @@ class Job < ApplicationRecord
   scope :is_open, -> { where(closed_at: nil) }
   scope :is_closed, -> { where.not(closed_at: nil) }
 
+  def open?
+    closed_at.nil?
+  end
+
+  def closed?
+    !open?
+  end
+
   def self.with_tags(*args)
     tagged_with(args, any: true)
   end
--- test/factories/jobs.rb
+++ test/factories/jobs.rb
@@ -1,12 +1,12 @@
 FactoryGirl.define do
   factory :job do
-    title Faker::Lorem.characters(7)
-    source_url Faker::Internet.url
-    source Faker::Lorem.characters(7)
-    city Faker::Address.city
-    state Faker::Address.state
-    country Faker::Address.country
-    description Faker::Lorem.paragraph
+    title { Faker::Lorem.characters(7) }
+    source_url { Faker::Internet.url }
+    source { Faker::Lorem.characters(7) }
+    city { Faker::Address.city }
+    state { Faker::Address.state }
+    country { Faker::Address.country }
+    description { Faker::Lorem.paragraph }
     closed_at nil
     remote false
   end
