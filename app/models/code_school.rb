a448d701db0b2776d3e1080c786bd775bf745103
Alexandr Sorokin <alexandr52281@bk.ru>
2018-11-07 22:05:55 +0300

    Add valid scheme to CodeSchool url

--- app/models/code_school.rb
+++ app/models/code_school.rb
@@ -2,4 +2,22 @@ class CodeSchool < ApplicationRecord
   validates :name, :url, :logo, presence: true
   validates_inclusion_of :full_time, :hardware_included, :has_online, :online_only, :in => [true, false]
   has_many :locations, -> { order('state ASC, city ASC') }, dependent: :destroy
+
+  before_create :set_valid_scheme, :if => Proc.new{ |cs| URI(cs.url).scheme.nil? }
+
+  private
+
+  def set_valid_scheme
+    uri = URI::parse(self.url)
+    if uri.scheme.nil? && uri.host.nil?
+      unless uri.path.nil?
+        uri.scheme = "https"
+        uri.host = uri.path
+        uri.path = ""
+        HTTParty.get(uri.to_s, { timeout: 2 }).code rescue uri.scheme = "http"
+      end
+    end
+
+    self.url = uri.to_s
+  end
 end
