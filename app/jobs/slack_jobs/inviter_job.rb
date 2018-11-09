7074653b1cf84e31ebc0e5e9ab917b8f6385b6e3
wimo7083 <wimo7083@colorado.edu>
2018-11-08 22:54:36 -0700

    add sidekiq to scope and add context to sentry for errors

--- app/jobs/slack_jobs/inviter_job.rb
+++ app/jobs/slack_jobs/inviter_job.rb
@@ -1,11 +1,16 @@
 class SlackJobs
   class InviterJob < SlackJobs
+    include Sidekiq::Worker
+
     def perform(email)
       # Invites user to general channel
+      Raven.user_context email: email
+      Raven.tags_context slack_invite: 'yes'
       slack_client.invite(
-        email: email, 
+        email: email,
         channels: ['C03GSNF77'],
-        extra_message: "Thank you for joining operation code, please continue signup")
+        extra_message: 'Welcome to Operation Code, please continue your journey into our online community!'
+      )
     end
   end
 end
