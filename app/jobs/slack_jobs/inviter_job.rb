6cfebb66d37ed3491443bd1b0625ae826dedaf6f
wimo7083 <wimo7083@colorado.edu>
2018-11-09 23:53:50 -0700

    change message

--- app/jobs/slack_jobs/inviter_job.rb
+++ app/jobs/slack_jobs/inviter_job.rb
@@ -9,7 +9,10 @@ class SlackJobs
       slack_client.invite(
         email: email,
         channels: ['C03GSNF77'],
-        extra_message: 'Welcome to Operation Code, please continue your journey into our online community!'
+        extra_message: 'Welcome to Operation Code!\n'\
+        'Once you log in, please take a moment to introduce yourself in the #general channel - a bit about who you are, where you\'re coming from, and where you\'re interested in going next.\n'\
+        'Thank you for joining us!\n'\
+        '-from the OC volunteer team\n'\
       )
     end
   end
