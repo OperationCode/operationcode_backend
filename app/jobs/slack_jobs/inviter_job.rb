e394193a5ef61900200b1f20008de9df237695fb
markchernov <mark.chernov@gmail.com>
2018-11-12 16:40:27 -0500

    changed auto-join slack channel

--- app/jobs/slack_jobs/inviter_job.rb
+++ app/jobs/slack_jobs/inviter_job.rb
@@ -8,7 +8,7 @@ class SlackJobs
       Raven.tags_context slack_invite: 'yes'
       slack_client.invite(
         email: email,
-        channels: ['C03GSNF77'],
+        channels: ['C1T66M57G'],
         extra_message: 'Welcome to Operation Code!\n'\
         'Once you log in, please take a moment to introduce yourself in the #general channel - a bit about who you are, where you\'re coming from, and where you\'re interested in going next.\n'\
         'Thank you for joining us!\n'\
