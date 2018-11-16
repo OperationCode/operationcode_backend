875df08d6abcc47bea264064bd149617edc10979
wimo7083 <wimo7083@colorado.edu>
2018-11-16 12:57:06 -0700

    fix ending

--- app/jobs/slack_jobs/inviter_job.rb
+++ app/jobs/slack_jobs/inviter_job.rb
@@ -9,7 +9,7 @@ class SlackJobs
       slack_client.invite(
         email: email,
         channels: ['C1T66M57G'],
-        extra_message: 'Welcome to Operation Code! After logging in, please introduce yourself in the #general channel - a bit about who you are, where you\'re coming from, and where you\'re interested in going next.
+        extra_message: 'Welcome to Operation Code! After logging in, please introduce yourself in the #general channel - a bit about who you are, where you\'re coming from, and where you\'re interested in going next.'
       )
     end
   end
