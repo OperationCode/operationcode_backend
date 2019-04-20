80577f24375e43e15c164c7b342651e6c78c95e0
wimo7083 <wimo7083@colorado.edu>
2019-04-20 00:37:32 -0600

    fix linting

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
   config.log_level = :debug
 
   # Prepend all log lines with the following tags.
-  config.log_tags = [ :request_id ]
+  config.log_tags = [:request_id]
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
@@ -90,5 +90,5 @@ Rails.application.configure do
   }
   config.action_mailer.default_url_options = { host: 'operationcode.org' }
 
-  Raven.configure {  |config| config.dsn = "https://#{ENV['SENTRY_CREDENTIALS']}@sentry.io/147247" }
+  Raven.configure { |config| config.dsn = "https://#{ENV['SENTRY_CREDENTIALS']}@sentry.io/147247" }
 end
