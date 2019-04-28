781d5c63e3c1cf90482ed2528be77cd5680bdd0e
Julian <hello@juliantrueflynn.com>
2019-04-28 01:39:04 -0400

    Remove previous Slack client that is no longer necessary

--- app/jobs/slack_jobs.rb
+++ app/jobs/slack_jobs.rb
@@ -1,12 +1,2 @@
-require 'slack/client'
-
 class SlackJobs < ApplicationJob
-  private
-
-  def slack_client
-    @slack_client ||= Slack::Client.new(
-      subdomain: ENV.fetch('SLACK_SUBDOMAIN'),
-      token:     ENV.fetch('SLACK_LEGACY_ADMIN_TOKEN') # admin token required to invite
-    )
-  end
 end
