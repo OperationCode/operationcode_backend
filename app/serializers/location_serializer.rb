08fb323c19a0578fe008e04eb3af0c988096c86d
github username <github email address>
2019-10-05 15:54:39 -0400

    Added housing_provided column to locations table, defaulting to false
    Created a new migration to add a housing_provided column to the locations table, with a default value of false. Also added housing_provided to the locations serializer so that it is rendered in the JSON returned from the API.
    
    This commit is meant to solve issue #397. It will allow housing data to be stored in the back end for each location.

--- app/serializers/location_serializer.rb
+++ app/serializers/location_serializer.rb
@@ -1,3 +1,3 @@
 class LocationSerializer < ActiveModel::Serializer
-  attributes :va_accepted, :address1, :address2, :city, :state, :zip
+  attributes :va_accepted, :address1, :address2, :city, :state, :zip, :provides_housing
 end
